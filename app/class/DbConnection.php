34da326177f1b955633aeff441d4aa18e4204a7d
felix-hauger <felix.hauger@laplateforme.io>
2023-03-10 19:37:47 +0100

    DbConnection class - get PDO static property from db.ini with singleton model class

--- app/class/DbConnection.php
+++ app/class/DbConnection.php
@@ -2,5 +2,29 @@
 
 class DbConnection
 {
-    
+    private static ?PDO $_db = null;
+
+    private function __construct()
+    {
+        // singleton
+    }
+
+    public static function getDb()
+    {
+        
+        if (!self::$_db) {
+            try {
+                // get database infos from ini file in config folder
+                $db = parse_ini_file('..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'db.ini');
+
+                self::$_db = new PDO($db['type'] . ':dbname=' . $db['name'] . ';host=' . $db['host'] . ';charset=utf8mb4', $db['user'], $db['password']);
+
+                self::$_db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+
+            return self::$_db;
+        }
+    }
 }
