8d2fdfd9b83bca1fef341f71c5daeed0e00cc823
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-03-10 21:43:59 +0100

    PHP, Class User: Method isValid & cheCk_DB

--- app/class/User.php
+++ app/class/User.php
@@ -1,6 +1,43 @@
 <?php
-
+namespace User;
+require_once('./DbConnection.php');
 class User
 {
+    private ?string $username;
+    private ?string $email;
+    private ?string $password;
+    private ?string $repass;
+    private ?array $data;
+
+
+    /**
+     * method of securing inputs
+     * @return string.
+     */
+    public function isValid(string $input)
+    {
+
+        $input = trim($input);
+        $input = htmlspecialchars($input);
+        $input = strip_tags($input);
+
+        return $input;
+    }
+
+    /**
+    * @return ?array[0]["$data"]
+    */
+    public function check_DB($username)
+    {
+
+        $requestCheck = "SELECT * FROM `users` WHERE username=:username";
+        $data = \DbConnection::getDb()->prepare($requestCheck);
+        $data->bindParam(':username', $username);
+        $data->execute();
+        return $data->fetchAll(\PDO::FETCH_ASSOC);
+    }
     
 }
+$user = new User();
+
+var_dump($user->check_DB('mohamed'));
