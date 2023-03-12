4cfedffa3301cb507072657d2b86ab68780b2508
felix-hauger <felix.hauger@laplateforme.io>
2023-03-12 17:21:46 +0100

    vue/src/ - renamed article.html in article.php

---
+++ vue/src/article.php
@@ -0,0 +1,27 @@
+<?php 
+require_once './../../app/controller/article.php';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./../../public/img/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="./../../public/style/style.css">
+    <link rel="stylesheet" href="./../../public/style/article.css">
+    <script defer src="./../js/article.js"></script>
+    <title><?php  ?> | Blog Cuisine</title>
+</head>
+<body>
+    <?php require_once("./../includes/header.php") ?>
+
+    <main>
+
+        
+        
+    </main>
+
+    <?php require_once("./../includes/footer.php") ?>
+</body>
+</html>
