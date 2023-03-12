e9357a51ef2116b4531100bb39b29cd85c61dee1
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-03-12 17:30:32 +0100

    PHP, controller, connect.php

---
+++ app/controller/connect.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+require_once './../class/User.php';
+$user = new User\User();
+
+if (isset($_POST['password']) && isset($_POST['username'])){
+
+    //******* */ validation inputs HTML.
+    // *********************************
+
+    $username = $user->isValid($_POST['username']);
+    $password = $user->isValid($_POST['password']);
+
+    if($user->connection($username,$password)):
+
+        // stored id and role in $_SESSIO
+        $_SESSION['id'] = $user->getId($username);
+        $_SESSION['role'] = $user->getRole($username);
+
+
+        //  change statusText HTTP.
+        header("HTTP/1.1 200 connected !");
+
+    endif;
+
+
+}
