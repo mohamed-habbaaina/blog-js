1b4cfe1ec33665e144fa420b1daee27ac13daa5a
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-03-13 12:04:37 +0100

    PHP, controller decconect

---
+++ app/controller/deconnect.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+require_once('./../class/User.php');
+$user = new User\User();
+if(!$user->isConnected()):
+    header('location: ./../../public/index.php');
+endif;
+
+$user->deconnect();
+header('location: ./../../public/index.php');
