b3fcb90c7037b3141317436256ea3e327c12b397
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-03-16 06:31:00 +0100

    PHP, class Admin, method: getAllArticles()

--- app/class/Admin.php
+++ app/class/Admin.php
@@ -7,11 +7,44 @@ class Admin extends \User\User {
 private ?array $data;
 
 
-public function getAllUsers(){
+public function getAllUsers(): array
+{
     $reqAllUsers = "SELECT * FROM `users`";
     $dataAllUsers = DbConnection::getDb()->prepare($reqAllUsers);
     $dataAllUsers->execute();
     return $dataAllUsers->fetchAll(PDO::FETCH_ASSOC);
+}
+
+public function deleteUser($username): void
+{
+    $reqDe = "DELETE FROM `users` WHERE username=:username";
+    $reqDelete = DbConnection::getDb()->prepare("$reqDe");
+    $reqDelete->bindParam(":username", $username);
+    $reqDelete->execute();
+
+
+}
+
+public function getAllArticles(): array
+{
+    $reqArtcl = "SELECT title, creation_date, username, articles.id FROM `articles` INNER JOIN `users` ON users.id = articles.user_id";
+    $dataArticl = DbConnection::getDb()->prepare("$reqArtcl");
+    $dataArticl->execute();
+    return $dataArticl->fetchAll(PDO::FETCH_ASSOC);
+}
+
+
+/**
+ * update the user's role
+ */
+public function updateRole(string $username, string $role): void
+{
+    $req = "UPDATE `users` SET `role`=':role' WHERE username=:username";
+    $reqRole = DbConnection::getDb()->prepare("$req");
+    $reqRole->bindParam(':username', $username);
+    $reqRole->bindParam(':role', $role);
+    $reqRole->execute();
+
 }
 
 public function getUsername($username)
