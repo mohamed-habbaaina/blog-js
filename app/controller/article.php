3dddbd6df0e1ae244556fbc796c7f46c5baf2507
felix-hauger <felix.hauger@laplateforme.io>
2023-03-17 15:18:10 +0100

    PHP, controller/article.php - insert comment in db, add http code 201 then refresh to vue/src/article.php

--- app/controller/article.php
+++ app/controller/article.php
@@ -1,7 +1,28 @@
 <?php
 
+require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'Comment.php';
+
+session_start();
 // if (!isset($_GET['id'])) {
     // header('Location: blog.html');
     // die();
 // }
 
+if (isset($_POST['submit-comment'])) {
+    $add_comment = new Comment();
+
+    $content = htmlspecialchars(trim($_POST['comment-content']), ENT_QUOTES);
+    $article_id = htmlspecialchars(trim($_POST['article-id']), ENT_QUOTES);
+
+    if (!empty($content) && !empty($article_id)) {
+        $add_comment->setContent($content)
+            ->setArticleId($article_id)
+            ->setUserId($_SESSION['id']);
+
+        if($add_comment->create()) {
+            header("HTTP/1.1 201 Created");
+            header('Refresh: 0; url=../../vue/src/article.php?id=' . $article_id);
+            die();
+        }
+    }
+}
