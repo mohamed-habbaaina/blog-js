47a6e510e7fb52b72e59a98b95aec7095c5f5939
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-03-19 13:52:36 +0100

    controller: blog & admin page category

--- app/controller/admincategory.php
+++ app/controller/admincategory.php
@@ -1,5 +1,21 @@
 <?php
-
 require_once('./../class/Admin.php');
 
+$admin = new Admin();
+$response = array();
+$response['status'] = 400;
 
+$data = json_decode(file_get_contents('php://input'), true);
+
+if(isset($data['namecategorie']) && isset($data['description'])):
+
+    $name = $data['namecategorie'];
+    $description = $data['description'];
+
+    $admin->insertCaregory($name, $description);
+
+    $response['status'] = 201;
+
+endif;
+
+echo (json_encode($response));
