d4778cd33efbb9fe39d280048bdd1d85e017b720
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-03-23 10:10:46 +0100

    PHP, model, get last 3 articles

---
+++ app/controller/home.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+require_once('./../class/Home.php');
+
+// Get 3 last articles.
+
+
+$articles = new Home();
+$articles = $articles->getArticlHome();
+
+echo json_encode($articles);
