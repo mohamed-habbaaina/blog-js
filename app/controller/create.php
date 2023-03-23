4b5f746f642d4d76240f833ebb481355c070f5aa
felix-hauger <felix.hauger@laplateforme.io>
2023-03-23 16:38:25 +0100

    PHP, app/controller/create.php - moved get_image_file() function to functions.php file in vue/includes folder

--- app/controller/create.php
+++ app/controller/create.php
@@ -2,6 +2,7 @@
 require_once dirname(__DIR__) . '/class/Article.php';
 require_once dirname(__DIR__) . '/class/Category.php';
 require_once dirname(__DIR__) . '/class/User.php';
+require_once dirname(dirname(__DIR__)) . '/vue/includes/functions.php';
 
 session_start();
 
@@ -17,52 +18,6 @@ $title = htmlspecialchars(trim($_POST['title']), ENT_QUOTES);
 $content = htmlspecialchars(trim($_POST['content']), ENT_QUOTES);
 $category = htmlspecialchars(trim($_POST['category']), ENT_QUOTES);
 
-function get_image_file($image_file, $name, $destination_path) {
-    // test if file exists and has no error
-    if (isset($image_file) && $image_file['error'] === 0) {
-
-        // limit image size
-        if ($image_file['size'] > 2000000) {
-            throw new Exception('Taille de l\'image maximum : 2mo');
-        } else {
-            // get image infos
-            $image_infos = pathinfo($image_file['name']);
-
-            // get image extensions
-            $image_extension = $image_infos['extension'];
-
-            // accepted extensions array
-            $extensions_array = ['png', 'gif', 'jpg', 'jpeg', 'webp'];
-
-            if (in_array($image_extension, $extensions_array)) {
-                // name image using $name parameter with image extension
-                $image_name = $name . '.' . $image_infos['extension'];
-
-                // set path to using $destination_path parameter with image name
-                $image_path = $destination_path . $image_name;
-
-                // attempt to move image file to image folder
-                if(move_uploaded_file($image_file['tmp_name'], $image_path)) {
-                    // if successful, return image name to set it in Article instance, then at image column in articles table
-                    return $image_name;
-                }
-            } else {
-                // if the format is not accepted
-                $extensions_string = '';
-
-                foreach ($extensions_array as $key => $extension) {
-                    if ($key === array_key_last($extensions_array)) {
-                        $extensions_string .= $extension;
-                    } else {
-                        $extensions_string .= $extension . ', ';
-                    }
-                }
-                throw new Exception('Format du fichier non accepté. Formats acceptés : ' . $extensions_string);
-            }
-        }
-    }
-}
-
 if (!empty($title) && !empty($content) && !empty($category)) {
     try {
         $article = new Article();
@@ -75,6 +30,7 @@ if (!empty($title) && !empty($content) && !empty($category)) {
         // set destination path to article thumbnail folder
         $path = dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . 'article_thumbnail' . DIRECTORY_SEPARATOR;
 
+        // required function get_image_file() to send image $file to $path and return its $name concatenated with its extension
         $image = get_image_file($file, $name, $path);
 
         $article->setTitle($title)
---
+++ vue/includes/functions.php
@@ -0,0 +1,48 @@
+<?php
+
+// send image $file to $path and return its $name concatenated with its extension
+function get_image_file($image_file, $name, $destination_path) {
+    // test if file exists and has no error
+    if (isset($image_file) && $image_file['error'] === 0) {
+
+        // limit image size
+        if ($image_file['size'] > 2000000) {
+            throw new Exception('Taille de l\'image maximum : 2mo');
+        } else {
+            // get image infos
+            $image_infos = pathinfo($image_file['name']);
+
+            // get image extensions
+            $image_extension = $image_infos['extension'];
+
+            // accepted extensions array
+            $extensions_array = ['png', 'gif', 'jpg', 'jpeg', 'webp'];
+
+            if (in_array($image_extension, $extensions_array)) {
+                // name image using $name parameter with image extension
+                $image_name = $name . '.' . $image_infos['extension'];
+
+                // set path to using $destination_path parameter with image name
+                $image_path = $destination_path . $image_name;
+
+                // attempt to move image file to image folder
+                if(move_uploaded_file($image_file['tmp_name'], $image_path)) {
+                    // if successful, return image name to be stored in instance and/or database
+                    return $image_name;
+                }
+            } else {
+                // if the format is not accepted
+                $extensions_string = '';
+
+                foreach ($extensions_array as $key => $extension) {
+                    if ($key === array_key_last($extensions_array)) {
+                        $extensions_string .= $extension;
+                    } else {
+                        $extensions_string .= $extension . ', ';
+                    }
+                }
+                throw new Exception('Format du fichier non accepté. Formats acceptés : ' . $extensions_string);
+            }
+        }
+    }
+}
