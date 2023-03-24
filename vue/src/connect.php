d915a64024d46582866e3585e6d8f97cd4dfedac
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-03-24 11:41:27 +0100

    CSS, connect page

--- vue/src/connect.php
+++ vue/src/connect.php
@@ -8,6 +8,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="./../../public/img/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="./../../public/style/style.css">
     <link rel="stylesheet" href="./../../public/style/connect.css">
     <script defer src="./../js/connect.js"></script>
     <title>Connection</title>
@@ -23,13 +24,11 @@ session_start();
             
             <label for="username">Username</label>
             <input type="username" name="username" placeholder="Entre Votre Username">
-            <small></small>
             
             <label for="password">Password</label>
             <input type="password" name="password" placeholder="Entre Votre Password">
-            <small></small>
             
-            <button>Connexion</button>
+            <button class="btn">Connexion</button>
 
         </form>
         
