be61050a7c8711305bd56d90d7eac0e77ae37524
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-03-24 12:22:01 +0100

    css, profil page

--- vue/src/profil.php
+++ vue/src/profil.php
@@ -17,7 +17,8 @@ $username = $_SESSION['username'];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="./../../public/img/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="./../../public/style/register.css">
+    <link rel="stylesheet" href="./../../public/style/style.css">
+    <link rel="stylesheet" href="./../../public/style/connect.css">
     <script defer src="./../js/profil.js"></script>
     <title>Profil</title>
 </head>
@@ -42,7 +43,7 @@ $username = $_SESSION['username'];
             <input type="password" name="repass" placeholder="Confirmer Votre Password">
             <small></small>
             
-            <button>Envoyer</button>
+            <button class="btn">Envoyer</button>
         </form>
         
     </main>
