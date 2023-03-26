bf70f8cba35834030bf75d3a37c84e133f4eb5ab
felix-hauger <felix.hauger@laplateforme.io>
2023-03-26 14:54:34 +0200

    HTML, vue/includes/footer.php - added link for icon source

--- vue/includes/footer.php
+++ vue/includes/footer.php
@@ -1,4 +1,4 @@
 <footer>
     <a class="btn" href="https://github.com/mohamed-habbaaina/blog-js">Lien GITHUB</a>
-    <p>origine icône : freepik</p>
+    <p>origine icône : <a href="https://www.flaticon.com/fr/icones-gratuites/chef" title="chef icônes">freepik</a></p>
 </footer>
